316842a359182c1a10d0efdb95ea7b5bf7e633d6
kevinfarukh <kevinfarukh@gmail.com>
2022-03-31 22:43:39 -0500

    create user

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,33 @@
+const fs = require("fs")
+const path = require("path")
+const bcrypt = require("bcryptjs")
+const usersFilePath = path.join(__dirname, '../data/usersData.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const userController = {
     index:(req, res) =>{
         res.send("user")
+    },
+    create:(req,res)=>{
+        res.render("users/formularioCrearUsuario")
+    },
+    createPost:(req,res)=>{
+        //info
+        let newUser = {
+            id: Date.now(),
+            userName: req.body.userName,
+            email: req.body.email,
+            password: bcrypt.hashSync (req.body.password, 10),
+            img: req.file.filename
+        };
+        users.push(newUser)
+
+        let userJSON=JSON.stringify(users);
+
+		fs.writeFileSync(usersFilePath, userJSON);
+		
+        //redirigir
+        res.redirect("/")
     }
 }
 
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const { append } = require("express/lib/response");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const userController = require("../controllers/usersController");
 
+let storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "./public/images/users");
+    },
+    filename: function(req,file,cb){
+        cb(null,`${Date.now()}_img_${path.extname(file.originalname)}`)
+    }
+})
+let upload = multer({ storage })
+
 router.get("/", userController.index)
+
+//creación
+router.get("/create",userController.create);
+router.post("/create",upload.single("img"),userController.createPost);
 
 module.exports = router
 
